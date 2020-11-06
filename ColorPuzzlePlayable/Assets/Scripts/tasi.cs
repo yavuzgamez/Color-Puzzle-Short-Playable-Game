322f6425709f4b4313bc383414ceb09eaa385e56
yavuzgamez <74048938+yavuzgamez@users.noreply.github.com>
2020-11-06 15:34:55 +0300

    Project is imported

---
+++ ColorPuzzlePlayable/Assets/Scripts/tasi.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tasi : MonoBehaviour
+{
+    Camera kamera;
+    Vector2 baslangic_pozisyonu;
+    GameObject[] kutu_dizisi;
+    yonetici yonet;
+    private void OnMouseDrag()
+    {
+        Vector3 pozisyon = kamera.ScreenToWorldPoint(Input.mousePosition);
+        pozisyon.z = 0;
+        transform.position = pozisyon;
+    }
+
+    private void Start()
+    {
+        kamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        baslangic_pozisyonu = transform.position;
+        kutu_dizisi = GameObject.FindGameObjectsWithTag("kutu");
+        yonet = GameObject.Find("yonetici").GetComponent<yonetici>();
+     
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            foreach(GameObject kutu in kutu_dizisi) 
+            { if (kutu.name == gameObject.name) 
+                {
+                    float mesafe = Vector3.Distance(kutu.transform.position, transform.position);
+                    if(mesafe<=1)
+                    {
+                        yonet.correct_place();
+                        transform.position = kutu.transform.position;
+                        yonet.sayi_arttir();
+                        this.enabled = false;
+                      
+                               }
+                    else
+                    {
+                        yonet.wrong_place();
+                        transform.position = baslangic_pozisyonu;
+                        
+
+                    }
+        
+                
+                }
+            }
+
+        }
+        
+    }
+}
---
+++ ColorPuzzlePlayable/Assets/Scripts/yonetici.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yonetici : MonoBehaviour
+
+{
+    [SerializeField]
+    private GameObject Clap;
+    [SerializeField]
+    private GameObject Tick;
+    [SerializeField]
+    private GameObject opage;
+    [SerializeField]
+    private GameObject mainmusic;
+    [SerializeField]
+    private GameObject gameover;
+    [SerializeField]
+    private GameObject Correct;
+    [SerializeField]
+    private GameObject Wrong;
+
+    int yerlestirilen_parca = 0;
+    int toplam_puzzle = 4;
+
+
+    void Start()
+    {
+        Tick.SetActive(false);
+        Clap.SetActive(false);
+        gameover.SetActive(false);
+        Correct.SetActive(false);
+        Wrong.SetActive(false);
+
+        StartCoroutine(WaitBeforeShow());
+        mainmusic.SetActive(true);
+
+    }
+
+    IEnumerator WaitBeforeShow()
+    {
+        opage.SetActive(true);
+        yield return new WaitForSeconds(3);
+        opage.SetActive(false);
+    }
+
+    IEnumerator WaitBeforeGameOver()
+    {
+        gameover.SetActive(false);
+        yield return new WaitForSeconds(4);
+        gameover.SetActive(true);
+    }
+
+
+
+    IEnumerator CorrectSetToFalse()
+    {
+        Correct.SetActive(true);
+        yield return new WaitForSeconds(1);
+        Correct.SetActive(false);
+    }
+
+
+    IEnumerator WrongSetToFalse()
+    {
+        Wrong.SetActive(true);
+        yield return new WaitForSeconds(1);
+        Wrong.SetActive(false);
+    }
+
+
+    public void correct_place()
+    {
+        StartCoroutine(CorrectSetToFalse());
+    }
+
+    public void wrong_place()
+    {
+        StartCoroutine(WrongSetToFalse());
+    }
+
+
+
+
+    public void sayi_arttir()
+    {
+        yerlestirilen_parca++;
+         if (yerlestirilen_parca==toplam_puzzle)
+        {
+
+            Tick.SetActive(true);
+            Clap.SetActive(true);
+            StartCoroutine(WaitBeforeGameOver());
+
+        }
+             
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
